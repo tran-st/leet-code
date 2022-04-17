a72cceab63fa24b3410c08960d98bad2e3f85ff5
Steven Tran <steventran9102@gmail.com>
2022-04-17 16:28:12 +0700

    graphs/course_schedule.py

--- graphs/course_schedule.py
+++ graphs/course_schedule.py
@@ -41,4 +41,55 @@ def dfs(course_map, course_set, course):
 numCourses = 5
 prerequisites = [[0, 1], [0, 2], [1, 3], [1, 4], [3, 4]]
 
-print(canFinish(numCourses, prerequisites))
+# print(canFinish(numCourses, prerequisites))
+
+
+def canFinish2(numCourses, prerequisites):
+    course_hash = { i:[] for i in range(numCourses) }
+    course_set = set()
+
+    for c, p in prerequisites: # Fill course/prerequisite hash
+        course_hash[c].append(p)
+
+    def dfs(course):
+        # Two base cases
+        if course in course_set:
+            return False
+
+        if course_hash[course] == []:
+            return True
+
+        course_set.add(course)
+
+        for p in course_hash[course]:
+            if not dfs(p):
+                return False
+
+            # Dfs success
+        course_set.remove(course)
+        course_hash[course] = []
+
+        return True
+
+    for i in range(numCourses):
+        if not dfs(i):
+            return False
+            
+
+    return True
+
+"""
+Input: numCourses = 2, prerequisites = [[1,0],[0,1]]
+Output: false
+Explanation: There are a total of 2 courses to take. 
+To take course 1 you should have finished course 0, and to take course 0 you should also have finished course 1. So it is impossible.
+
+Approach:
+
+DFS. Store course and prerequisites into hash.
+"""
+
+numCourses = 4
+prerequisites = [[2,0],[1,0],[3,1],[3,2],[1,3]]
+
+print(canFinish2(numCourses, prerequisites))
