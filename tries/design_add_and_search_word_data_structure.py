a757c92c2494624f4fd98e09ca5235c243f4dee2
Steven Tran <steventran9102@gmail.com>
2022-04-24 15:08:50 +0700

    tries/design_add_and_search_word_data_structure.py

---
+++ tries/design_add_and_search_word_data_structure.py
@@ -0,0 +1,58 @@
+class TrieNode:
+    def __init__(self):
+        self.children = {}
+        self.endOfWord = False
+
+class WordDictionary:
+
+    def __init__(self):
+        self.root = TrieNode()
+
+    def addWord(self, word: str) -> None:
+        current = self.root
+
+        for char in word:
+            if char not in current.children:
+                current.children[char] = TrieNode()
+
+            current = current.children[char]
+
+        current.endOfWord = True
+        
+
+    def search(self, word: str) -> bool:
+        def dfs(j, node):
+            current = node
+
+            for i in range(j, len(word)):
+                char = word[i]
+
+                if char == ".":
+                    for child in current.children.values():
+                        print(child)
+                        if dfs(i + 1, child):
+                            return True
+
+                    return False
+                else:
+                    if char not in current.children:
+                        return False
+
+                    current = current.children[char]
+
+            return current.endOfWord
+
+        return dfs(0, self.root)
+        
+
+
+# Your WordDictionary object will be instantiated and called as such:
+# obj = WordDictionary()
+# obj.addWord(word)
+# param_2 = obj.search(word)
+
+obj = WordDictionary()
+obj.addWord("bad")
+obj.addWord("dad")
+obj.addWord("mad")
+print(obj.search("mad"))
