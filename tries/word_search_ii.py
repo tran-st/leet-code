ac9d32162d167fd3ebcc6a05738f2f526b73bb07
Steven Tran <steventran9102@gmail.com>
2022-04-24 15:09:54 +0700

    tries/word_search_ii.py

---
+++ tries/word_search_ii.py
@@ -0,0 +1,92 @@
+class TrieNode:
+    def __init__(self):
+        self.children = {}
+        self.isEnd = False
+
+    def addWords(self, word):
+        current = self
+
+        for char in word:
+            if char not in current.children:
+                current.children[char] = TrieNode()
+
+            current = current.children[char] # Increment
+
+        current.isEnd = True
+
+def findWords(board, words):
+    row_length = len(board)
+    col_length = len(board[0])
+    visited = set()
+    result = set()
+    directions = [(-1,0), (1,0), (0,-1), (0,1)]
+
+    root = TrieNode()
+
+    for word in words: # Add word list to trie
+        root.addWords(word)
+
+    def dfs(row, col, node, word):
+
+        if (row not in range(row_length) or
+            col not in range(col_length) or
+            # (row, col) in visited or
+            board[row][col] not in node.children):
+            return
+
+        char = board[row][col]
+
+        visited.add((row, col))
+        word += char # Valid char, add
+        nextNode = node.children[char] # Progress trie
+
+        if nextNode.isEnd: # Word found
+            result.add(word)
+            nextNode.isEnd = False
+
+            if not node.children.keys():
+                del node.children[char]
+
+        for x, y in directions:
+            dfs(row+x, col+y, nextNode, word)
+
+        visited.remove((row,col))
+
+    for i in range(row_length):
+        for j in range(col_length):
+            dfs(i, j, root, "")
+
+    return result
+
+"""
+Input: board = [["o","a","a","n"],
+                ["e","t","a","e"],
+                ["i","h","k","r"],
+                ["i","f","l","v"]], 
+                
+                words = ["oath","pea","eat","rain"]
+Output: ["eat","oath"]
+
+Approach:
+
+Iterate each word, each char. See if grid char match. Check all 4 directions
+
+Time    : O(cnm4^nm)
+Space   : O(1)
+
+
+Approach 2:
+
+Add each word to a trie. For each grid char see if char exists in trie. Traverse trie
+
+Time    : O(cnm)
+"""
+
+board = [["o","a","b","n"],
+         ["o","t","a","e"],
+         ["a","h","k","r"],
+         ["a","f","l","v"]]
+
+words =["oa","oaa"]
+
+print(findWords(board, words))
