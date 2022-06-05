8a3f8f67e90aff9d757d80693d33aa7ac68af460
Steven Tran <steventran9102@gmail.com>
2022-06-05 15:12:42 +0700

    graphs/graph_valid_tree.py

--- graphs/graph_valid_tree.py
+++ graphs/graph_valid_tree.py
@@ -6,7 +6,7 @@ def valid_tree(n, edges):
         if node1 not in adjacency:
             adjacency[node1] = []
 
-        if node2 not in adjacency:
+        if node2 not in adjacency: 
             adjacency[node2] = []
 
         adjacency[node1].append(node2)
@@ -21,7 +21,7 @@ def valid_tree(n, edges):
         for j in adjacency[i]:
             if j == prev: # Can ignore traversing to previous elements
                 continue
-            if not dfs(j):
+            if not dfs(j, i):
                 return False
 
         return True
@@ -44,3 +44,49 @@ n = 5
 edges = [[0, 1], [0, 2], [0, 3], [1, 4]]
 
 print(valid_tree(n, edges))
+
+"""
+def valid_tree(n, edges):
+    // Empty tree technically valid
+    if not n:
+        return True 
+
+    // Initialize adjacency list
+    adjacency = { i:[] for i in range(n) }
+
+    // Fill adjacency list
+    for vertex, edge in edges:
+        adjacency[vertex].append(edge)
+        adjacency[edge].append(vertex)
+
+    // Track visited
+    visited = set()
+
+    def dfs(i, prev):
+        // Loop detected
+        if i in visited:
+            return False
+
+        visited.add(i)
+
+        // Visit all children
+        for vertex in adjacency[i]:
+            // Don't visit if previous
+            if vertex == prev:
+                continue
+            if not dfs(vertex, i):
+                return False
+
+    return dfs(0, -1) and n == len(visited)
+
+Approach:
+
+Try to visit each node. Check for loops. Track previous
+
+Time    : O(E + V)
+Space   : O(E + V)
+
+Takeaway:
+
+Understand graph traversal, dfs
+"""
